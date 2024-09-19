41da2f62ee752db5b07581588c1cbd4921db0355
Ian Lebbern <ianlebbern@gmail.com>
2024-09-19 13:04:36 -0400

    Update streamlit_app.py
    Pandas import - planning to use SEARCH_ON column for API

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -14,7 +15,13 @@ name_on_order = st.text_input("Name on Smoothie: ")
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+my_dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
